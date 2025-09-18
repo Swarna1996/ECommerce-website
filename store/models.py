8bcbf2af294905ec5235088520c3eec4d05ec377
Swarna Bandara <swarnalevis@gmail.com>
2025-09-17 23:48:27 -0400

    Register new users

--- store/models.py
+++ store/models.py
@@ -12,7 +12,7 @@ class Category(models.Model):
         return self.name
     
     class Meta:
-        verbose_name_plural = 'categories'
+        verbose_name_plural = 'categories' #otherwise django plurals this as categorys
 
 
 
--- store/views.py
+++ store/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render,redirect
 from .models import Product
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
+from django import forms
 
 # Create your views here.
 def home(request):
@@ -32,3 +36,22 @@ def logout_user(request):
     logout(request)
     messages.success(request,("You have been logged out successfully"))
     return redirect('home')
+
+def register_user(request):
+    form = SignUpForm()
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            #login user
+            user = authenticate(username=username,password=password)
+            login(request,user)
+            messages.success(request,("Registered successfully!"))
+            return redirect('home')
+        else:
+            messages.error(request,("Something went wrong"))
+            return redirect('register')
+    else:
+        return render(request,'register.html',{'form':form})
