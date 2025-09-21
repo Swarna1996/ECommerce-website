4563d8a1b2b916e828689ea39310919f88de8b5b
Swarna Bandara <swarnalevis@gmail.com>
2025-09-21 14:16:47 -0400

    Update shopping cart button

--- cart/views.py
+++ cart/views.py
@@ -22,8 +22,12 @@ def cart_add(request):
         #save to session
         cart.add(product=product)
 
+		# Get Cart Quantity
+        cart_quantity = cart.__len__()
+
         #Return response
-        response = JsonResponse({'Product Name: ':product.name})
+        # response = JsonResponse({'Product Name: ':product.name})
+        response = JsonResponse({'qty': cart_quantity})
         return response
 
 def cart_delete(request):
