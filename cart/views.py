3227a04142d6a1e5e3dfad5155b247fcb0d164f1
Swarna Bandara <swarnalevis@gmail.com>
2025-09-23 21:31:32 -0400

    Cart messages Added

--- cart/views.py
+++ cart/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,get_object_or_404
 from .cart import Cart
 from store.models import Product
 from django.http import JsonResponse
+from django.contrib import messages
 
 # Create your views here.
 
@@ -36,6 +37,7 @@ def cart_add(request):
         #Return response
         # response = JsonResponse({'Product Name: ':product.name})
         response = JsonResponse({'qty': cart_quantity})
+        messages.success(request,('Product Added to the Cart'))
         return response
 
 def cart_delete(request):
@@ -47,6 +49,7 @@ def cart_delete(request):
         cart.delete(product=product_id)
 
         response = JsonResponse({'product':product_id})
+        messages.success(request,('Product Deleted from Cart'))
         return response
 
 def cart_update(request):
@@ -60,5 +63,6 @@ def cart_update(request):
         cart.update(product=product_id, quantity=product_qty)
 
         response = JsonResponse({'qty':product_qty})
+        messages.success(request,('Product Updated'))
         return response
         
